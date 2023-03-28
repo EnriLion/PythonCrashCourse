fe8ac3de64017fe1169adbfc9e246fe1a6cdbfff
EnriLion <enriqueleon0201leon@gmail.com>
2023-03-27 18:07:23 -0600

    Updating repository

--- pythoncrashbook/part1Basics/p5/more_conditional_test.py
+++ pythoncrashbook/part1Basics/p5/more_conditional_test.py
@@ -106,6 +106,7 @@ for company in companies:
 
 companies = ['marvel', 'dc']
 
+print('\n')
 
 for company in companies:
     if company == 'dc':
@@ -113,4 +114,40 @@ for company in companies:
     else:
         print(f'Batman is the best superheroe in {company}')
 
+#More conditional tests
+
+print("\n","6")
+
+pets = ['fish', 'dog', 'cat', 'lion']
+
+
+if 'cow' not in pets: 
+        Not_in = 'cow' not in pets
+        print(f'The cows cant be a mascot  and that is {Not_in}')
+else:
+        print(f'The cows cant be a mascot  and that is {Not_in}')
+
+    
+pets = ['fish', 'dog', 'cat', 'lion']
+
+
+Is_in = 'serpient' in pets
+
+print("\n")
+
+if Is_in == False: 
+        print(f'The serpient can be a mascot  and that is {Is_in}')
+else:
+        print(f'The cows cant be a mascot  and that is {Not_in}')
+
+print("\nTest for equality and inequality with string")
+
+Cat = 'maine coon'
+
+Other_cat = Cat == 'british'
+
+if Other_cat == Cat: 
+    print(f"Is cat == 'british' ? I predict {Other_cat}.")
+else:
+    print(f"Is cat == 'british' ? I predict {Other_cat}.")
 
