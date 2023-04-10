fa9fce9251efe652b38f9f239394db730db1b41e
EnriLion <enriqueleon0201leon@gmail.com>
2023-04-10 18:46:58 -0500

    Updating repositories

---
+++ pythoncrashbook/part1Basics/p7/counting.py
@@ -0,0 +1,17 @@
+#Introducing the while loops
+
+#the for loop takes a collection of items and executes the block of code once(if a condition is true)
+
+#The while loop in action
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+#A progam uses a lot or contain while loops for example, a game needs a while loop to keep running as long as you want to keep playing and so it can stop running as soon as you ask it to quit
+
+#while loop s are quite useful
+
+
+
--- pythoncrashbook/part1Basics/p7/parrot.py
+++ pythoncrashbook/part1Basics/p7/parrot.py
@@ -6,3 +6,20 @@ message = input("Tell me something , and I will repeat it back to you: ")
 print(message)
 
 #Note: Sublime Text and many other editors don't run programs that prompt the user for input. You can use these editors to write programs that prompt for input, but you'll need to run these programs from a terminal.
+
+#Letting the user choose when to quit
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt +="\nEnter 'quit' to end the program: "
+
+message = ""
+
+while message != 'quit':
+    message = input(prompt)
+    #This program works well, except that it prints the word 'quit' as if it were an actual message a simple if test fices this:
+    #now the porgram makes a quick check before displaying the message and only prints the message if it does not match the quit value:
+    print(message)
+
+#Using a flag
+
+
