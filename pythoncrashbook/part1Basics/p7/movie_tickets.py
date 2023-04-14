92352219673244150e7c3c30702de5ddd12aa0b0
EnriLion <enriqueleon0201leon@gmail.com>
2023-04-13 20:39:24 -0500

    Updating files

--- pythoncrashbook/part1Basics/p7/movie_tickets.py
+++ pythoncrashbook/part1Basics/p7/movie_tickets.py
@@ -1,19 +1,19 @@
 
 
 prompt = "\nWhat is your age?"
-prompt +="\nAnd I will give you the charge"
+prompt +="\nAnd I will give you the charge :"
 
 
-flag = True
-
-while flag:
-    message = int() 
-    message = input(prompt)
-    if message == 3:
-        print("Your ticket is free")
-        flag = False
-    if message == 12:
-        print("Your ticket is $10")
-        flag = False
-
-   
+while True:
+    message = int(input(prompt))
+    if message <= 3:
+        charge = "free"  
+        break
+    elif message > 3 and message <= 12:
+        charge = 10 
+        break
+    elif message > 12:
+        charge = 15
+        break
+print(f"\nThe cost of the movie is {charge} ")
+        
