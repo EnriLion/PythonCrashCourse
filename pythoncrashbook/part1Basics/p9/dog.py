9d7f73b9ae82c376159adfa9117e3bf037f0b8c4
EnriLion <enriqueleon0201leon@gmail.com>
2023-04-29 00:56:23 -0600

    Updating files

---
+++ pythoncrashbook/part1Basics/p9/dog.py
@@ -0,0 +1,7 @@
+#Object oriented programming  is one of the most effective approaches to writing software
+
+#You could create objects and classes
+
+##Creating and using a class
+
+#You can bame it almost everything using classes  writing a simple class dog and represents a dog 
