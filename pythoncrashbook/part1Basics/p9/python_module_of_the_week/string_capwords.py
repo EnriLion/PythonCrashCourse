22ebe44106c60e34b70a97cb69296cc5dd4b295a
EnriLion <enriqueleon0201leon@gmail.com>
2023-05-12 17:03:32 -0600

    Updating file

--- pythoncrashbook/part1Basics/p9/python_module_of_the_week/string_capwords.py
+++ pythoncrashbook/part1Basics/p9/python_module_of_the_week/string_capwords.py
@@ -1,6 +1,7 @@
-
 #Functions
 ##string - Text Constants and Templates
+
+#Python modules: https://pymotw.com/3/index.html
 
 #The string module dates from the earluer version of Python and have been moved to the methods str objects. The string module retains several useful constants and  classes for working with str objects
 
