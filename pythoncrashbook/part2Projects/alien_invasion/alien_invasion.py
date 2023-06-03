e93666a09e19610abcb7b077607f615a93e1b33a
EnriLion <enriqueleon0201leon@gmail.com>
2023-06-03 13:30:35 -0600

    Updating files and updating alien project

--- pythoncrashbook/part2Projects/alien_invasion/alien_invasion.py
+++ pythoncrashbook/part2Projects/alien_invasion/alien_invasion.py
@@ -1,46 +1,127 @@
+##Creating a Pygame Window and Responding to User Input
+#import sys
+
+#import pygame
+
+#from settings import Settings
+#from ship import Ship
+
+#class AlienInvasion:
+#    """Overall class to manage assets and behavior."""
+
+#    def __init__(self):
+#        """Initialize the game, and create a game resources"""
+#        pygame.init() #initializes the background settings that Pygame needs to work properly
+
+#        self.settings = Settings()# We create an instance of Settions and assing it to self.settings
+
+#        # self.screen = pygame.display.set_mode((1200, 800))#we make a variable self.screen(will be available in all methods in the class) to create a display with a tuple that defines the dimensions of the game window which be 1200 pixels wide and 800 high
+#        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))#we ccreate a screen with the attributes of the class settings
+#        pygame.display.set_caption("Alien Invasion")
+#        #--Drawing the Ship to the Screen--
+
+#        self.ship = Ship(self)#the call to Ship() requires one argument, an instance of AlienInvasion and the self argument here refers to the current instance of AlienInvasion with this parameter that gives Ship acces to the game's resouces, such as the screen object. We assign this Ship instance of self.ship
+
+#        #--Setting the background color--
+
+#        #Set the background color.
+#        # self.bg_color = (230, 230, 230)# show the variants of the RGB colors green(0, 255, 0), blue(0, 0, 255), red(255, 0, 0)
+
+#    def run_game(self):#this method contains a while lopp
+#        """Start the main loop for the game."""
+#        while True:
+#            # Watch for keyboard and mouse events.
+#            for event in pygame.event.get():#we write an event loop to listen for events and perform appropriate tasks dependending on the kinds of events that occur any keyboard or mouse event will cause this for loop to run.
+#                if event.type == pygame.QUIT:# detect and respond to specific events( for ex when the player clicks the game windows; close a pygame.QUIT event is detected and we call sys.exit() to exit the game
+#                    sys.exit()
+
+#            #--Setting the background color--
+#            # Redraw the screen during each pass through the loop.
+#            # self.screen.fill(self.bg_color)#we fill the screen the backgroundusing the fill() method, which acts on a surface and takes only one argument: a color
+#            self.screen.fill(self.settings.bg_color)# instead of creating the background color in the __init__ class we make it with inheritance of the class settings accession to the variable self.bg_color()
+
+#            #--Drawing the Ship to the Screen--
+
+#            self.ship.blitme()#After the filling the background, we draw the ship on the screen by calling ship.blitme() so the ship appears on top of the background
+
+#            # Make the most recently drawn screen visible.
+#            pygame.display.flip()# we call it to make the most recently drawn screen visible and is simply drawn an empty screen on each pass through the while loop erasing elements around pygame.display.flip() continually updates the display to show the new positions of game elements and hides the old ones, creating the illusion of smooth movement.
+
+#if __name__ == '__main__':
+#    # Make a game instance, and run the game.
+#    ali = AlienInvasion()
+#    ali.run_game()
+##At the end of the file we create an isntance of the game, and then call run_game()( in an if block that only runs the file is called directly.
+
 #Creating a Pygame Window and Responding to User Input
 import sys
 
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create a game resources"""
-        pygame.init() #initializes the background settings that Pygame needs to work properly
+        pygame.init()
 
-        self.settings = Settings()# We create an instance of Settions and assing it to self.settings
-
-        # self.screen = pygame.display.set_mode((1200, 800))#we make a variable self.screen(will be available in all methods in the class) to create a display with a tuple that defines the dimensions of the game window which be 1200 pixels wide and 800 high
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))#we ccreate a screen with the attributes of the class settings
+        self.settings = Settings()
+ 
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        #--Drawing the Ship to the Screen--
+
+        self.ship = Ship(self)
 
         #--Setting the background color--
 
         #Set the background color.
         # self.bg_color = (230, 230, 230)# show the variants of the RGB colors green(0, 255, 0), blue(0, 0, 255), red(255, 0, 0)
 
-    def run_game(self):#this method contains a while lopp
+    def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():#we write an event loop to listen for events and perform appropriate tasks dependending on the kinds of events that occur any keyboard or mouse event will cause this for loop to run.
-                if event.type == pygame.QUIT:# detect and respond to specific events( for ex when the player clicks the game windows; close a pygame.QUIT event is detected and we call sys.exit() to exit the game
-                    sys.exit()
+            #--Refactoring: The _check_events() and _update_screen() methods--
+            #The _check_events() Method
 
-            #--Setting the background color--
-            # Redraw the screen during each pass through the loop.
-            # self.screen.fill(self.bg_color)#we fill the screen the backgroundusing the fill() method, which acts on a surface and takes only one argument: a color
-            self.screen.fill(self.settings.bg_color)# instead of creating the background color in the __init__ class we make it with inheritance of the class settings accession to the variable self.bg_color()
+            self._check_events()#we call the method form inside the while loop un run_game()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()# we call it to make the most recently drawn screen visible and is simply drawn an empty screen on each pass through the while loop erasing elements around pygame.display.flip() continually updates the display to show the new positions of game elements and hides the old ones, creating the illusion of smooth movement.
+            #The _update_screen() Method
+
+            self._update_screen()
+
+
+
+
+#--Refactoring: The _check_events() and _update_screen() methods--
+
+    def _check_events(self):#we make a method  whther the player has clicked to close the window in this new method
+        """Respond to keypress and mouse events."""
+        #The _check_events() Method
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):#
+        """Update images on the screen, and flip to the new screen."""
+        #--Setting the background color--
+        self.screen.fill(self.settings.bg_color)
+        #--Drawing the Ship to the Screen--
+        self.ship.blitme()
+        #--Drawing the Ship to the Screen--
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ali = AlienInvasion()
     ali.run_game()
 #At the end of the file we create an isntance of the game, and then call run_game()( in an if block that only runs the file is called directly.
+
+#It is better for us breaking our coude in methods and refactoring our code.
+
+
+
---
+++ pythoncrashbook/part2Projects/alien_invasion/ship.py
@@ -0,0 +1,28 @@
+#Creating the Ship Class
+import pygame
+
+#We import  the pygame module before defining the class. The__init__() method of Ship takes two parameters: the self reference and a reference to the current instance of the alienInvasion.
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set it s starting position."""
+        self.screen = ai_game.screen#we assign the screen to an attribute of Ship, so we can access it eaily in all  the methods in this class
+        self.screen_rect = ai_game.screen.get_rect()# we access the screen's rect attribute using the get_rect() method and assign it to self.screen_rect. Doing so allow us to place the ship in the correct location on the scren
+
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp') #we call the method load pygame.image.load() and give the location of our image and we assgin it to a variable self.image
+        self.rect = self.image.get_rect()#we call get_rect() to access the ship surface's rect attribute so we can later use it to place the ship
+
+        #Load the ship image and get its rect.
+        self.rect.midbottom = self.screen_rect.midbottom# We position the ship at the bottom center of the screen to do we make the value self.rect.midbottom match the midbootom attribute of the screen'srect
+
+    def blitme(self):#we define the blime() mehtod, which draws the image to the scren at the positino specified by self.rect
+        """Draw the ship at its curren tlocation."""
+        self.screen.blit(self.image, self.rect)
+
+#Pygame is efficient because it lets you treat all game elements like rectangles(rect)
+
+#When we are working with a rect object we can use the x-and y-coordinates of the top, bottom. left and right edges of the rectangle as well as the center( we coul add values as center, centerx, cetnery attributes or if we are working at an edge of the screen as top, bottom, left or right) and also are attributes that combine those properties as midbottom, midtop, midleft and midright). These attributes spare us from having to do calculations that game developers formerly had to do manually, and you'll use them often.
+
+#Note: Pygame the origin(0, 0)is at the top-left corner of the screen, and coordinates increase as you go down and to the right. On a 1200 by 800 screen, the origin is at the top-left corner, and the bottom-right corner has the coordinates(1200, 800). These coordiantes refer to the game window, now the physical screen.
