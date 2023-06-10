0613a487dc89b6f62b6cdec5873fba07d8b5c5e2
EnriLion <enriqueleon0201leon@gmail.com>
2023-06-09 23:47:26 -0600

    Updating files

---
+++ pythoncrashbook/part2Projects/exercises/rocket/rocket.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Rocket:
+    """A class to manage the rocket."""
+
+    def __init__(self, re_game):
+        """Initialize the rocket and set its starting position."""
+        self.screen = re_game.screen
+        self.screen_rect = re_game.screen.get_rect()
+
+        # Load the rocket image and get its rect.
+        self.image = pygame.image.load('images/rocketland.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new rocket at the bottom center of the screen.
+        # self.rect.midbottom = self.screen_rect.midbottom#1
+        self.rect.center = self.screen_rect.center
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down= False
+
+    def update(self):
+        """Update the rocket position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
+        elif self.moving_up:
+            self.rect.y -= 1
+        elif self.moving_down:
+            self.rect.y += 1
+
+    def blitme(self):
+        """Draw the rocket at its current location."""
+        self.screen.blit(self.image, self.rect)
+
---
+++ pythoncrashbook/part2Projects/exercises/rocket/rocket_exercise.py
@@ -0,0 +1,79 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from rocket import Rocket
+
+class RocketExercise:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create gam resources."""
+        pygame.init()
+        self.settings = Settings()
+
+        # self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Rocket Exercise")
+
+        self.rocket = Rocket(self)
+
+        # self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            self.rocket.update()
+
+
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    # self.rocket.rect.x += 1
+                    self.rocket.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the up
+                    # self.rocket.rect.x -= 1
+                    self.rocket.moving_left = True
+                elif event.key == pygame.K_UP:
+                    # Move the ship to the up
+                    # self.rocket.rect.y -= 1
+                    self.rocket.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    # Move the ship to the up
+                    # self.rocket.rect.y += 1
+                    self.rocket.moving_down = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.rocket.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.rocket.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.rocket.moving_down = False
+
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.rocket.blitme()
+
+        # Make the  most recently drawn screen visible
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    re = RocketExercise()
+    re.run_game()
