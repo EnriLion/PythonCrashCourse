70f6d9d081ceb6acbed8fbcea5d23a17f159c1b7
EnriLion <enriqueleon0201leon@gmail.com>
2023-07-16 19:18:38 -0600

    Updating files

---
+++ kaggle/hello_python.py
@@ -0,0 +1,13 @@
+viking_song = "Spam " * 4
+spam_amount = 0
+spam_amount = spam_amount + 4
+viking_song = "Spam " * spam_amount # this is called operator overloading
+print(viking_song)
+
+# Numbers and arithmetic in Python
+
+#Basic calculations in python
+
+# a //b  - floor division - quotient of a and b, removing fractional parts
+# a % b  - modulus - quotient of a and b, removing fractional parts
+# a ** b  - exponentiation - a raised to the power of b
--- pythoncrashbook/part2Projects/exercises/part13/stars/space_game.py
+++ pythoncrashbook/part2Projects/exercises/part13/stars/space_game.py
@@ -32,18 +32,26 @@ class SpaceGame:
         number_stars_x = available_space_x // (2 * star_width)
 
         # Determine the number of rows of stars that fit on the screen.
+        available_space_y = (self.settings.screen_height - (3 * star_height))
+        number_rows = available_space_y // (2 * star_height)
 
 
-        # Create the first row of stars
-        for star_number in range(number_stars_x):
-            self._create_star(star_number)
+        # # Create the first row of stars
+        # for star_number in range(number_stars_x):
+        #     self._create_star(star_number)
 
-    def _create_star(self, star_number):
+        # Create the full fleet of stars.
+        for row_number in range(number_rows):
+            for star_number in range(number_stars_x):
+                self._create_star(star_number, row_number)
+
+    def _create_star(self, star_number, row_number):
         # Create a star and place it in the row.
         star = Star(self)
-        star_width = star.rect.width
-        star.x = star_width +2 * star_width * star_number
+        star_width, star_height = star.rect.size
+        star.x = star_width + 2 * star_width * star_number
         star.rect.x = star.x
+        star.rect.y = star.rect.height + 2 * star.rect.height * row_number
         self.stars.add(star)
 
     def run_game(self):
