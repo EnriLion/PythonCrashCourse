305e39654b5bec07d5a2abba67ee1fcee06a9992
r <r>
2023-08-18 21:49:00 -0600

    Updating files

--- pythoncrashbook/part2Projects/data_visualization/mpl_squares.py
+++ pythoncrashbook/part2Projects/data_visualization/mpl_squares.py
@@ -2,9 +2,22 @@ import  matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
 
-fig, ax = plt.subplots()
 
-ax.plot(squares)
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+# the thickness of the line that plot() generates
+
+# Set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+# The set_title method sets a title for the chart
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=24)
+
+#The methods allow you to set a title for each of the axes
+
+# Set size of tick labels.
+ax.tick_params(axis='both', labelsize=14)
+# The method tick_params() styles the tick marks
 
 plt.show()
 
