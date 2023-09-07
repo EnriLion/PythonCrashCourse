bf4818e33c0a248e90370027bce118850634802e
r <r>
2023-09-06 20:40:44 -0600

    Updating files

--- pythoncrashbook/part2Projects/exercises/part15/molecular_motion.py
+++ pythoncrashbook/part2Projects/exercises/part15/molecular_motion.py
@@ -6,14 +6,15 @@ from random_walk import RandomWalk
 while True:
     # Make a random walk.
     # rw = RandomWalk()
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5000)
     # Importing the RandomWalk store it in rw
     rw.fill_walk()
     # we call fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15,9), dpi=128)
+    # dpi set a plot size that makes effective use of the space available
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
     # We use range()=generate list of number equal to numbepoints in the walk.
