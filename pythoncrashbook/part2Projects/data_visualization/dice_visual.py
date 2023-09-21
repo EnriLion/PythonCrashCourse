0633525e0780e5c3032ec03cddb65c2a6c77390b
r <r>
2023-09-20 21:24:57 -0600

    Updating files

--- pythoncrashbook/part2Projects/data_visualization/dice_visual.py
+++ pythoncrashbook/part2Projects/data_visualization/dice_visual.py
@@ -3,7 +3,9 @@ from plotly import offline
 from die import Die
 
 # Create a D6
-die = Die()
+# die = Die()
+die_1 = Die()
+die_2 = Die()
 # we create  an instace of Die with the default six sides
 
 # Make some rolls, and store results in a list.
@@ -11,14 +13,16 @@ results = []
 for roll_num in range(100):
     # We roll the die 100 times and store each roll in th elist results
     # We're not longer printing the results, we can increase the number of stimulated rolls to 1000
-    result = die.roll()
+    # result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # print(results)
 # Analyze the results.
 frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
 # To analyze the rolls, we create the empty list frequencies
-for value in range(1, die.num_sides+1):
+for value in range(1, max_result+1):
     # We loop through the possible values(1 thorugh 6)
     frequency = results.count(value)
     # how many times each number appears in results
@@ -27,10 +31,20 @@ for value in range(1, die.num_sides+1):
 # print(frequencies)
 
 # Visualize the results.
-x_values = list(range(1, die.num_sides+1))
+# x_values = list(range(1, die.num_sides+1))
+x_values = list(range(2, max_result+1))
+# we need a bar for each of the possible  and we store these in a list called x_values(starts at 1 and ends at the number of sides on the die plotly doesn't accept the results of the range() function directly
+# so we convert the range to a list() function
 data = [Bar(x=x_values, y=frequencies)]
+# The Bar() represents a data set will be formatted as a bar chart
+# And the class needs  a list of x_values / y_values
+# The class must be wrapped in square backets can have more elements.
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
+# x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
 my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+# We put the titles on the variable as well as offline.pot(needs a..
+# dictionary containig data and layout objects also accepts a name
+# for the file where the graph will be save(d6.html)
