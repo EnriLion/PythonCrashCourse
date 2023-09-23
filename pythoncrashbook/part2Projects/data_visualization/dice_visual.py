6a6d274c364ac1034861ce23e9e5a8bbdbcbc59c
r <r>
2023-09-22 19:20:25 -0600

    Updating repository

--- pythoncrashbook/part2Projects/data_visualization/dice_visual.py
+++ pythoncrashbook/part2Projects/data_visualization/dice_visual.py
@@ -4,13 +4,17 @@ from die import Die
 
 # Create a D6
 # die = Die()
+# die_1 = Die()
+# die_2 = Die()
+# Create a D6 and a D10
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 # we create  an instace of Die with the default six sides
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(100):
+# for roll_num in range(100):
+for roll_num in range(50_000):
     # We roll the die 100 times and store each roll in th elist results
     # We're not longer printing the results, we can increase the number of stimulated rolls to 1000
     # result = die.roll()
@@ -47,8 +51,10 @@ data = [Bar(x=x_values, y=frequencies)]
 x_axis_config = {'title': 'Result', 'dtick': 1}
 # x_axis_config = {'title': 'Result'}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+# my_layout = Layout(title='Results of rolling one D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+my_layout = Layout(title='Results of rolling a D6 and a D10 50000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+# offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
 # We put the titles on the variable as well as offline.pot(needs a..
 # dictionary containig data and layout objects also accepts a name
 # for the file where the graph will be save(d6.html)
